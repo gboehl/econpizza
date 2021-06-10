2fee9c1fb9bfd3bd4b7d924f73694aff8be980d7
Gregor Boehl <admin@gregorboehl.com>
2021-06-10 19:41:05 +0200

    some doc

--- econpizza/shooting.py
+++ econpizza/shooting.py
@@ -4,8 +4,9 @@
 import yaml
 import re
 import warnings
-from numpy import *
+import time
 import numpy as np
+from numpy import log, exp, sqrt
 import matplotlib.pyplot as plt
 import scipy.optimize as so
 from numba import njit
@@ -20,6 +21,11 @@ def parse(mfile, verbose=True):
     mtxt = mtxt.replace("^", "**")
     mtxt = re.sub(r"@ ?\n", " ", mtxt)
     model = yaml.safe_load(mtxt)
+
+    defs = model.get("definitions")
+    if defs is not None:
+        for d in defs:
+            exec(d, globals())
 
     evars = model["variables"]
     shocks = model.get("shocks") or ()
@@ -47,16 +53,19 @@ def parse(mfile, verbose=True):
     else:
         shock_str = shocks[0] + " = shocks[0]"
 
-    func_str = """def func_raw(XLag, X, XPrime, XSS, shocks, pars):\n %s\n %s\n %s\n %s\n %s\n %s\n F=np.empty(%s)\n %s\n %s\n return F""" % (
-        ", ".join(v + "Lag" for v in evars) + " = XLag",
-        ", ".join(evars) + " = X",
-        ", ".join(v + "Prime" for v in evars) + " = XPrime",
-        ", ".join(v + "SS" for v in evars) + " = XSS",
-        shock_str,
-        ", ".join(par.keys()) + " = pars",
-        str(len(evars)),
-        "\n ".join(eqns_aux) if eqns_aux else "",
-        "\n ".join(eqns),
+    func_str = (
+        """def func_raw(XLag, X, XPrime, XSS, shocks, pars):\n %s\n %s\n %s\n %s\n %s\n %s\n F=np.empty(%s)\n %s\n %s\n return F"""
+        % (
+            ", ".join(v + "Lag" for v in evars) + " = XLag",
+            ", ".join(evars) + " = X",
+            ", ".join(v + "Prime" for v in evars) + " = XPrime",
+            ", ".join(v + "SS" for v in evars) + " = XSS",
+            shock_str,
+            ", ".join(par.keys()) + " = pars",
+            str(len(evars)),
+            "\n ".join(eqns_aux) if eqns_aux else "",
+            "\n ".join(eqns),
+        )
     )
 
     try:
@@ -153,15 +162,14 @@ def solve_current(model, XLag, XPrime, tol):
     stst = model.get("stst")
     shocks = model.get("shocks") or ()
 
-    def func_current(x):
-        return func(
-            XLag,
-            x,
-            XPrime,
-            np.array(list(stst.values())),
-            np.zeros(len(shocks)),
-            np.array(list(par.values())),
-        )
+    func_current = lambda x: func(
+        XLag,
+        x,
+        XPrime,
+        np.array(list(stst.values())),
+        np.zeros(len(shocks)),
+        np.array(list(par.values())),
+    )
 
     res = so.root(func_current, XPrime, options=model["root_options"])
     err = np.max(np.abs(func_current(res["x"])))
@@ -174,16 +182,61 @@ def find_path(
     x0,
     T=30,
     init_path=None,
-    max_horizon=500,
+    max_horizon=200,
+    max_loops=100,
     max_iter=None,
     tol=1e-5,
+    reverse=False,
     root_options=None,
     debug=False,
     verbose=True,
 ):
+    """Find the expected trajectory given an initial state. A good strategy is to first set `tol` to a low value (e.g. 1e-3) and check for a good max_horizon. Then, set max_horizon to a reasonable value and let max_loops be high.
+
+    Parameters
+    ----------
+    model : dict
+        model dict as defined/parsed above
+    x0 : array
+        initial state
+    T : int, optional
+        number of periods to simulate
+    init_path : array, optional
+        a first guess on the trajectory. Normally not necessary
+    max_horizon : int, optional
+        number of periods until the system is assumed to be back in the steady state. A good idea to set this corresponding to the respective problem
+    max_loops : int, optional
+        number of repetitions to iterate over the whole trajectory. Should eventually be high.
+    max_iterations : int, optional
+        number of iterations. Default is `max_horizon`. It should not be lower than that (and will raise an error). Normally it should not be higher, better use `max_loops` instead.
+    tol : float, optional
+        convergence criterion
+    reverse : bool, optional
+        whether to start each iteration with the values most far in the future. Normally not a good idea.
+    root_options : dict, optional
+        dictionary with solver-specific options to be passed on to `scipy.optimize.root`
+    debug : bool, optional
+        debug mode. Currently not functional
+    verbose : bool, optional
+        degree of verbosity. 0/`False` is silent
+
+    Returns
+    -------
+    array
+        array of the trajectory
+    flag
+        integer of error flag
+    """
+
+    st = time.time()
 
     if max_iter is None:
         max_iter = max_horizon
+    elif max_iter < max_horizon:
+        Exception(
+            "max_iter should be higher or equal max_horizon, but is %s and %s."
+            % (max_iter, max_horizon)
+        )
 
     stst = list(model["stst"].values())
     evars = model["variables"]
@@ -211,7 +264,9 @@ def find_path(
     try:
         for i in range(T):
 
-            cnt = 2
+            loop = 1
+            cnt = 2 - reverse
+            old_clock = time.time()
             flag = np.zeros(5, dtype=bool)
 
             while True:
@@ -220,6 +275,10 @@ def find_path(
                 imax = min(cnt, max_horizon)
 
                 for t in range(imax):
+
+                    if reverse:
+                        t = imax - t - 1
+
                     x[t + 1], flag_root, flag_ftol = solve_current(
                         model, x[t], x[t + 2], tol
                     )
@@ -228,10 +287,25 @@ def find_path(
                 flag[1] |= np.any(np.isnan(x))
                 flag[2] |= np.any(np.isinf(x))
                 flag[3] |= cnt == flag_ftol
-                flag[4] |= cnt == max_iter
+
+                if cnt == max_iter:
+                    if loop < max_loops:
+                        loop += 1
+                        cnt = 2
+                    else:
+                        flag[4] |= True
 
                 fin_flag |= flag
                 err = np.abs(x_old - x[1]).max()
+
+                clock = time.time()
+                if verbose and clock - old_clock > 1:
+                    old_clock = clock
+                    print(
+                        "Period{:>4d} | loop{:>5d} | iter.{:>5d} | error: {:>1.8e}".format(
+                            i, loop, cnt, err
+                        )
+                    )
 
                 if (err < tol and cnt > 2) or flag.any():
                     break
@@ -255,10 +329,11 @@ def find_path(
         ", ftol not reached in root finding",
         ", max_iter reached",
     )
-    mess = [i * bool(j) for i, j in zip(msgs, flag)]
+    mess = [i * bool(j) for i, j in zip(msgs, fin_flag)]
     fin_flag = 2 ** np.arange(5) @ fin_flag
 
     if verbose:
-        print("Pizza done%s." % "".join(mess))
+        duration = np.round(time.time() - st, 3)
+        print("Pizza done after %s seconds%s." % (duration, "".join(mess)))
 
     return x_fin, fin_flag

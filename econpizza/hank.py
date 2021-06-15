bd1ae5231a7ddc8aa7b755824b60596347fbb8fc
Gregor Boehl <admin@gregorboehl.com>
2021-06-15 11:42:44 +0200

    speedup hank

--- econpizza/hank.py
+++ econpizza/hank.py
@@ -5,71 +5,66 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numba import njit
 from interpolation import interp
-from grgrlib.njitted import histogram_weights
 
 
-@njit
-def egm_ces_one_asset(bs, par, tol=1e-8):
+@njit(cache=True, fastmath=True)
+def egm_ces_1asset(bs, par, max_iter=1000, tol=1e-8):
 
-    rb, rl, gamma, beta, p, x, y = par
-    gy = bs.copy()
-    g0 = bs.copy()
+    rb, rl, sigma, beta, p, inc_b, inc_g = par
+    x = np.vstack((bs, bs))
     cnt = 0
+    flag = False
+
+    rPrime = np.ones_like(bs) * rl
+    rPrime[bs < 0] = rb
+    r = np.empty_like(x)
 
     while True:
 
         cnt += 1
 
-        gy_old = gy
-        g0_old = g0
+        if cnt > max_iter:
+            flag = True
+            break
 
-        giy = lambda x: interp(gy, bs, x)
-        gi0 = lambda x: interp(g0, bs, x)
+        x_old = x.copy()
 
-        r = np.ones_like(bs) * rb
-        ry = r.copy()
-        r0 = r.copy()
-        r[bs >= 0] = rl
+        fg = interp(x[0], bs, bs)
+        fb = interp(x[1], bs, bs)
 
         c = (
             beta
-            * r
+            * rPrime
             * (
-                p * (y + x - giy(bs) + r * bs) ** -gamma
-                + (1 - p) * (x - gi0(bs) + r * bs) ** -gamma
+                p * (inc_g - fg + rPrime * bs) ** -sigma
+                + (1 - p) * (inc_b - fb + rPrime * bs) ** -sigma
             )
-        ) ** (1 / -gamma)
+        ) ** (1 / -sigma)
 
-        gy = bs + c - y - x
-        g0 = bs + c - x
-        ry[gy >= 0] = rl
-        r0[g0 >= 0] = rl
-        gy /= ry
-        g0 /= r0
+        x[0] = bs + c - inc_g
+        x[1] = bs + c - inc_b
 
-        erry = np.abs(gy - gy_old).max()
-        err0 = np.abs(g0 - g0_old).max()
-        err = max(err0, erry)
+        r[:] = rb
+        r[0][x[0] >= 0] = rl
+        r[1][x[1] >= 0] = rl
 
-        if err < tol:
+        x /= r
+
+        if np.abs(x - x_old).max() < tol:
             break
 
-    return gy, g0, c, cnt
+    return x, flag
 
 
-@njit
-def find_stst_dist(bins, g0, gy, bs, prob, weights=None):
-    # must be the funcs and the probs
+@njit(cache=True, fastmath=True)
+def find_stst_dist(bins, x, fx, prob, max_iter=1000, tol=1e-8, weights=None):
 
-    giy = lambda x: interp(gy, bs, x)
-    gi0 = lambda x: interp(g0, bs, x)
+    n = len(bins)
 
     if weights is None:
         wths = np.ones(n) / n
     else:
         wths = weights
-
-    n = len(bins)
 
     # chose new bins such that the original bins are approximately their midpoints
     new_bins = np.empty(n + 1)
@@ -78,25 +73,34 @@ def find_stst_dist(bins, g0, gy, bs, prob, weights=None):
     new_bins[1:-1] = bins[:-1] + diff
     new_bins[-1] = bins[-1] + diff[-1]
 
-    m = np.empty(n * 2)
-    w = m.copy()
+    grid = np.hstack((interp(x[1], fx, bins), interp(x[0], fx, bins)))
+    sorting_index = np.argsort(grid)
+    sorted_grid = grid[sorting_index]
+    bin_index = np.searchsorted(sorted_grid, new_bins, "left")
+
+    new_wths = np.empty_like(grid)
+
     cnt = 0
+    flag = False
 
     while True:
 
         cnt += 1
-        wths_old = wths.copy()
 
-        m[:n] = gi0(bins)
-        m[n:] = giy(bins)
-        w[:n] = (1 - prob) * wths
-        w[n:] = prob * wths
-
-        wths, _ = histogram_weights(m, bins=new_bins, weights=w)
-
-        err = np.abs(wths - wths_old).max()
-
-        if err < 1e-8:
+        if cnt > max_iter:
+            flag = True
             break
 
-    return bins, wths, cnt
+        old_wths = wths.copy()
+
+        new_wths[:n] = (1 - prob) * wths
+        new_wths[n:] = prob * wths
+
+        sorted_new_wths = new_wths[sorting_index]
+        cum_new_wths = np.hstack((np.zeros(1), sorted_new_wths.cumsum()))
+        wths = np.diff(cum_new_wths[bin_index])
+
+        if np.abs(wths - old_wths).max() < tol:
+            break
+
+    return wths, flag

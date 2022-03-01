d12cb6dfbed08019d352646f1421934b391ef5d0
Gregor Boehl <admin@gregorboehl.com>
2022-03-01 22:58:58 +0100

    fix minimial versions

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
 setup(
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    url="https://github.com/gboehl/econsieve",
+    url="https://github.com/gboehl/econpizza",
     name="econpizza",
-    version="0.1.1",
+    version="0.1.4",
     author="Gregor Boehl",
     author_email="admin@gregorboehl.com",
     description="Solve nonlinear perfect foresight models",
@@ -28,8 +28,8 @@ setup(
         "numba",
         "numpy",
         "scipy",
-        "grgrlib",
         "jax",
         "jaxlib",
+        "grgrlib>=0.1.2",
     ],
 )

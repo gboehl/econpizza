2d17228ec67e8ae41e491c0a116ae5b9ae5ef28e
Gregor Boehl <admin@gregorboehl.com>
2022-05-20 20:33:07 +0200

    informative output and better docs

--- econpizza/steady_state.py
+++ econpizza/steady_state.py
@@ -44,14 +44,17 @@ def solve_stst(model, tol_newton=1e-8, maxit_newton=30, tol_backwards=None, maxi
     except KeyError:
         pass
 
+    # get all necessary functions
     func_eqns = model['context']['func_eqns']
     func_backw = model['context'].get('func_backw')
     func_stst_dist = model['context'].get('func_stst_dist')
 
+    # get initial values for heterogenous agents
     decisions_output_init = model['init_run'].get('decisions_output')
     exog_grid_vars_init = model['init_run'].get('exog_grid_vars')
     init_vf = model.get('init_vf')
 
+    # get the actual steady state function
     func_stst_raw, func_backw_ext = get_func_stst_raw(
         par, func_pre_stst, func_backw, func_stst_dist, func_eqns, shocks, init_vf, decisions_output_init, exog_grid_vars_init, tol_backw=tol_backwards, maxit_backw=maxit_backwards, tol_forw=tol_forwards, maxit_forw=maxit_forwards)
 
@@ -80,9 +83,9 @@ def solve_stst(model, tol_newton=1e-8, maxit_newton=30, tol_backwards=None, maxi
         distSS, cnt_forwards = func_stst_dist(
             decisions_output, tol_forwards, maxit_forwards)
         if jnp.isnan(vfSS).any() or jnp.isnan(jnp.array(decisions_output)).any():
-            mess += f"Backward function returns 'NaN's. "
+            mess += f"Backward iteration returns 'NaN's. "
         elif jnp.isnan(jnp.array(distSS)).any():
-            mess += f"Forward returns 'NaN's. "
+            mess += f"Forward iteration returns 'NaN's. "
         if cnt_backwards == maxit_backwards:
             mess += f'Maximum of {maxit_backwards} backwards calls reached. '
         if cnt_forwards == maxit_forwards:
@@ -99,12 +102,12 @@ def solve_stst(model, tol_newton=1e-8, maxit_newton=30, tol_backwards=None, maxi
         rank = jnp.linalg.matrix_rank(jac)
         mess += f"Function has rank {rank}, for {jac.shape[0]} variables ({len(model['steady_state']['fixed_evalued'])} fixed). "
         if not res["success"]:
-            mess = f"Steady state not found (error is {err:1.2e}). {mess}"
+            mess = f"Steady state not found (error is {err:1.2e}). {res['message']} {mess}"
         else:
-            mess = f"Steady state error is {err:1.2e}. {mess}"
+            mess = f"Steady state error is {err:1.2e}. {res['message']} {mess}"
     elif verbose:
         duration = time.time() - st
-        mess += f"Steady state found in {duration:1.5g} seconds."
+        mess += f"Steady state found in {duration:1.5g} seconds. {res['message']}"
 
     print(f"(solve_stst:) {mess}")
 

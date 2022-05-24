f4324ac4e5b1d3309b70e8d1d2c103043e0930ff
Gregor Boehl <admin@gregorboehl.com>
2022-05-24 09:14:10 +0200

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description_content_type="text/x-rst",
     url="https://github.com/gboehl/econpizza",
     name="econpizza",
-    version="0.1.5",
+    version="0.1.6",
     author="Gregor Boehl",
     author_email="admin@gregorboehl.com",
     description="Solve nonlinear perfect foresight models",

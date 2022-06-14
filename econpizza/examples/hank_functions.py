fix sign error

econpizza/examples/hank_functions.py
@@ -20,7 +20,7 @@ def hh(Va_p, a_grid, skills_grid, w, n, T, R, beta, eis, frisch):
 
     # consumption can be readily obtained from MUC and MU of labor
     labor_inc = skills_grid[:, jnp.newaxis]*n*w
-    c_nextgrid = uc_nextgrid**-eis - labor_inc/(1 + 1/frisch)
+    c_nextgrid = uc_nextgrid**-eis + labor_inc/(1 + 1/frisch)
 
     # get consumption in grid space
     lhs = c_nextgrid - labor_inc + a_grid[jnp.newaxis, :] - T[:, jnp.newaxis]
@@ -37,7 +37,7 @@ def hh(Va_p, a_grid, skills_grid, w, n, T, R, beta, eis, frisch):
     a = jnp.where(a < a_grid[0], a_grid[0], a)
 
     # calculate new MUC
-    Va = R * (c + labor_inc/(1 + 1/frisch)) ** (-1 / eis)
+    Va = R * (c - labor_inc/(1 + 1/frisch)) ** (-1 / eis)
 
     return Va, a, c
 
63cfd7fcc661995f24f591bcd773d638acba5ee7
Gregor Boehl <admin@gregorboehl.com>
2022-07-05 19:51:42 +0200

    code restructuring

---
+++ econpizza/solvers/shooting.py
@@ -0,0 +1,206 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+import jax
+import numpy as np
+import jax.numpy as jnp
+import scipy.optimize as so
+from .solve_linear import find_path_linear
+
+
+def solve_current(model, shock, XLag, XLastGuess, XPrime, tol):
+    """Solves for one period.
+    """
+
+    func = jax.jit(model['context']["func_eqns"])
+    pars = jnp.array(list(model["parameters"].values()))
+    stst = jnp.array(list(model["stst"].values()))
+
+    def func_current(x): return func(XLag, x, XPrime, stst, shock, pars)
+
+    res = so.root(func_current, XLastGuess, options=model["root_options"])
+    err = jnp.max(jnp.abs(func_current(res["x"])))
+
+    return res["x"], not res["success"], err > tol
+
+
+def find_pizza(
+    model,
+    x0=None,
+    shock=None,
+    T=30,
+    init_path=None,
+    max_horizon=200,
+    max_loops=100,
+    max_iter=None,
+    tol=1e-5,
+    use_linear_guess=False,
+    root_options={},
+    raise_error=False,
+    verbose=True,
+):
+    """Find the expected trajectory given an initial state. A good strategy is to first set `tol` to a low value (e.g. 1e-3) and check for a good max_horizon. Then, set max_horizon to a reasonable value and let max_loops be high.
+
+    Parameters
+    ----------
+    model : dict
+        model dict or PizzaModel instance
+    x0 : array
+        initial state
+    shock : tuple, optional
+        shock in period 0 as in `(shock_name_as_str, shock_size)`
+    T : int, optional
+        number of periods to simulate
+    init_path : array, optional
+        a first guess on the trajectory. Normally not necessary
+    max_horizon : int, optional
+        number of periods until the system is assumed to be back in the steady state. A good idea to set this corresponding to the respective problem. Note that a horizon too far away may cause the accumulation of numerical errors.
+    max_loops : int, optional
+        number of repetitions to iterate over the whole trajectory. Should eventually be high.
+    max_iterations : int, optional
+        number of iterations. Default is `max_horizon`. It should not be lower than that (and will raise an error). Normally it should not be higher, better use `max_loops` instead.
+    tol : float, optional
+        convergence criterion
+    root_options : dict, optional
+        dictionary with solver-specific options to be passed on to `scipy.optimize.root`
+    verbose : bool, optional
+        degree of verbosity. 0/`False` is silent
+
+    Returns
+    -------
+    x_fin : array
+        array of the trajectory
+    x_lin : array or None
+        array of the trajectory based on the linear model. Will return None if the linear model is unknown
+    fin_flag : int
+        error code
+    """
+
+    st = time.time()
+
+    if max_iter is None:
+        max_iter = max_horizon
+    elif max_iter < max_horizon:
+        Exception(
+            "max_iter should be higher or equal max_horizon, but is %s and %s."
+            % (max_iter, max_horizon)
+        )
+
+    stst = np.array(list(model["stst"].values()))
+    nvars = len(model["variables"])
+    shocks = model.get("shocks") or ()
+
+    if root_options:
+        model["root_options"] = root_options
+
+    # precision of root finding should be some magnitudes higher than of solver
+    if "xtol" not in model["root_options"]:
+        model["root_options"]["xtol"] = min(tol / max_horizon, 1e-8)
+
+    x_fin = np.empty((T + 1, nvars))
+    x_fin[0] = list(x0) if x0 is not None else stst
+
+    x = np.ones((T + max_horizon + 1, nvars)) * stst
+    x[0] = x_fin[0]
+
+    x, x_lin = find_path_linear(
+        model, shock, T + max_horizon, x, use_linear_guess)
+    x_lin = x_lin[: T + 1] if x_lin is not None else None
+
+    if init_path is not None:
+        x[1: len(init_path)] = init_path[1:]
+
+    tshock = np.zeros(len(shocks))
+
+    fin_flag = np.zeros(5, dtype=bool)
+    old_clock = time.time()
+
+    msgs = (
+        ", root finding did not converge",
+        ", ftol not reached in root finding",
+        ", contains NaNs",
+        ", contains infs",
+        ", max_iter reached",
+    )
+
+    try:
+        for i in range(T):
+
+            loop = 1
+            cnt = 2
+            flag = np.zeros_like(fin_flag)
+
+            while True:
+
+                x_old = x[1].copy()
+                imax = min(cnt, max_horizon)
+
+                flag_loc = np.zeros(2, dtype=bool)
+
+                for t in range(imax):
+
+                    if not t and not i and shock is not None:
+                        tshock[shocks.index(shock[0])] = shock[1]
+                    else:
+                        tshock[:] = 0
+
+                    x[t + 1], flag_root, flag_ftol = solve_current(
+                        model, tshock, x[t], x[t + 1], x[t + 2], tol
+                    )
+
+                    flag_loc[0] |= flag_root
+                    flag_loc[1] |= flag_ftol and not flag_root
+
+                flag[2] |= np.any(np.isnan(x))
+                flag[3] |= np.any(np.isinf(x))
+
+                if cnt == max_iter:
+                    if loop < max_loops:
+                        loop += 1
+                        cnt = 2
+                    else:
+                        flag[4] |= True
+
+                err = np.abs(x_old - x[1]).max()
+
+                clock = time.time()
+                if verbose and clock - old_clock > 0.5:
+                    old_clock = clock
+                    print(
+                        "   Period{:>4d} | loop{:>5d} | iter.{:>5d} | flag{:>3d} | error: {:>1.8e}".format(
+                            i, loop, cnt, 2 ** np.arange(5) @ fin_flag, err
+                        )
+                    )
+
+                if (err < tol and cnt > 2) or flag.any():
+                    flag[:2] |= flag_loc
+                    if raise_error and flag.any():
+                        mess = [i * bool(j) for i, j in zip(msgs, flag)]
+                        raise Exception("Aborting%s" % "".join(mess))
+                    fin_flag |= flag
+                    break
+
+                cnt += 1
+
+            x_fin[i + 1] = x[1].copy()
+            x = x[1:].copy()
+
+    except Exception as error:
+        raise type(error)(
+            str(error)
+            + " (raised in period %s during loop %s for forecast %s steps ahead)"
+            % (i, loop, t)
+        ).with_traceback(sys.exc_info()[2])
+
+    fin_flag[1] &= not fin_flag[0]
+    mess = [i * bool(j) for i, j in zip(msgs, fin_flag)]
+    fin_flag = 2 ** np.arange(5) @ fin_flag
+
+    if verbose:
+        duration = np.round(time.time() - st, 3)
+        print("(find_path:) Pizza done after %s seconds%s." %
+              (duration, "".join(mess)))
+
+    return x_fin, x_lin, fin_flag
---
+++ econpizza/solvers/stacking.py
@@ -0,0 +1,162 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import jax
+import time
+import jax.numpy as jnp
+import scipy.sparse as ssp
+from grgrlib.jaxed import newton_jax, jacfwd_and_val, jacrev_and_val
+from .solve_linear import find_path_linear
+from ..parser.build_functions import *
+
+
+def find_stack(
+    model,
+    x0=None,
+    shock=None,
+    init_path=None,
+    horizon=250,
+    tol=None,
+    maxit=None,
+    use_linear_guess=True,
+    use_linear_endpoint=None,
+    use_jacrev=True,
+    verbose=True,
+    **solver_kwargs,
+):
+    """Find the expected trajectory given an initial state.
+
+    Parameters
+    ----------
+    model : dict
+        model dict or PizzaModel instance
+    x0 : array
+        initial state
+    shock : tuple, optional
+        shock in period 0 as in `(shock_name_as_str, shock_size)`
+    init_path : array, optional
+        a first guess on the trajectory. Should not be necessary
+    horizon : int, optional
+        number of periods until the system is assumed to be back in the steady state. A good idea to set this corresponding to the respective problem. A too large value may be computationally expensive. A too small value may generate inaccurate results
+    tol : float, optional
+        convergence criterion. Defaults to 1e-8
+    maxit : int, optional
+        number of iterations. Default is 30.
+    use_linear_guess : bool, optional
+        whether to use the linear impulse responses as an initial guess. Defaults to True if the linear LOM is known
+    use_linear_endpoint : bool, optional
+        whether to use the linear impulse responses as the final state. Defaults to True if the linear LOM is known
+    use_jacrev : bool, optional
+        whether to use reverse mode or forward mode automatic differentiation. By construction, reverse AD is faster, but does not work for all types of functions. Defaults to True
+    verbose : bool, optional
+        degree of verbosity. 0/`False` is silent
+    solver_kwargs : optional
+        any additional keyword arguments will be passed on to the solver
+
+    Returns
+    -------
+    x : array
+        array of the trajectory
+    x_lin : array or None
+        array of the trajectory based on the linear model. Will return None if the linear model is unknown
+    flag : bool
+        returns True if the solver was successful, else False
+    """
+
+    st = time.time()
+
+    stst = jnp.array(list(model["stst"].values()))
+    nvars = len(model["variables"])
+    pars = jnp.array(list(model["parameters"].values()))
+    shocks = model.get("shocks") or ()
+    # load functions
+    func_eqns = model['context']["func_eqns"]
+    func_backw = model['context'].get('func_backw')
+    func_dist = model['context'].get('func_dist')
+
+    tol = 1e-8 if tol is None else tol
+    maxit = 30 if maxit is None else maxit
+
+    x0 = jnp.array(list(x0)) if x0 is not None else stst
+    x = jnp.ones((horizon + 1, nvars)) * stst
+    x = x.at[0].set(x0)
+
+    x_init, x_lin = find_path_linear(
+        model, shock, horizon, x, use_linear_guess)
+
+    if use_linear_endpoint is None:
+        use_linear_endpoint = False if x_lin is None else True
+    elif use_linear_endpoint and x_lin is None:
+        print("(find_path_stacked:) Linear solution for the endpoint not available")
+        use_linear_endpoint = False
+
+    if init_path is not None:
+        x_init[1: len(init_path)] = init_path[1:]
+
+    zshock = jnp.zeros(len(shocks))
+    tshock = jnp.copy(zshock)
+    if shock is not None:
+        tshock = tshock.at[shocks.index(shock[0])].set(shock[1])
+        if model.get('distributions'):
+            print("(find_path_stacked:) Warning: shocks for heterogenous agent models are not yet fully supported. Use adjusted steady state values as x0 instead.")
+
+    endpoint = x_lin[-1] if use_linear_endpoint else stst
+
+    if model.get('distributions'):
+        vfSS = model['steady_state']['decisions']
+        distSS = jnp.array(model['steady_state']['distributions'])
+        decisions_outputSS = jnp.array(
+            model['steady_state']['decisions_output'])
+
+        # get values of func_eqns that are independent of the distribution
+        stst_in_t = stst[:, None]
+
+        def func_eqns_from_dist(dist, decisions_output): return func_eqns(
+            stst_in_t, stst_in_t, stst_in_t, stst, zshock, pars, dist[..., None], decisions_output[..., None])
+
+        _, mask_out_jvp = jax.jvp(func_eqns_from_dist, (distSS, decisions_outputSS), (
+            jnp.ones_like(distSS), jnp.ones_like(decisions_outputSS)))
+        mask_out = jnp.tile(mask_out_jvp.astype(bool), horizon-1)
+
+        # compile dist-specific functions
+        stacked_func_dist_raw = get_stacked_func_dist(pars, func_backw, func_dist, func_eqns, x0, stst,
+                                                      vfSS, distSS, zshock, tshock, horizon, nvars, endpoint, model.get('distributions'), shock)
+        stacked_func_dist = jax.jit(
+            stacked_func_dist_raw, static_argnames='full_output')
+        model['context']['stacked_func_dist'] = stacked_func_dist
+        dist_dummy = distSS[..., None]
+        decisions_output_dummy = decisions_outputSS[..., None]
+    else:
+        dist_dummy = decisions_output_dummy = []
+
+    stacked_func = get_stacked_func(pars, func_eqns, stst, x0, horizon, nvars,
+                                    endpoint, zshock, tshock, shock, dist_dummy, decisions_output_dummy)
+
+    if verbose:
+        print("(find_path_stacked:) Solving stack (size: %s)..." %
+              (horizon*nvars))
+
+    if model.get('distributions'):
+        stacked_func = get_combined_funcs(
+            stacked_func_dist, stacked_func, mask_out, use_jacrev)
+    res = newton_jax(stacked_func, x_init[1:-1].flatten(), None, maxit, tol,
+                     sparse=True, func_returns_jac=True, verbose=verbose, **solver_kwargs)
+
+    # calculate error
+    err = jnp.abs(res['fun']).max()
+    x = x.at[1:-1].set(res['x'].reshape((horizon - 1, nvars)))
+
+    mess = res['message']
+
+    if err > tol or not res['success']:
+        mess += f" Max. error is {err:1.2e}."
+        verbose = True
+
+    if verbose:
+        duration = time.time() - st
+        sucess = 'done' if res['success'] else 'FAILED'
+        print(
+            f"(find_path_stacked:) Stacking {sucess} after {duration:1.3f} seconds. " + mess)
+
+    return x, x_lin, not res['success']

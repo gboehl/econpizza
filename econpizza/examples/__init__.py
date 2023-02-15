5c10beafb023be7e33058c3a2a1bde5c37ded2bc
Gregor Boehl <admin@gregorboehl.com>
2023-02-15 19:52:28 +0100

    add examples init

---
+++ econpizza/examples/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+pth = os.path.dirname(__file__)
+
+nk = os.path.join(pth, "nk.yaml")
+bh = os.path.join(pth, "bh.yaml")
+dsge = os.path.join(pth, "dsge.yaml")
+hank = os.path.join(pth, "hank.yaml")
+hank_labor = os.path.join(pth, "hank_labor.yaml")
+hank2 = os.path.join(pth, "hank2.yaml")

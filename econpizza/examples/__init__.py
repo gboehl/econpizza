db7e903a9b7929434e138803721a8655fadd62a1
Gregor Boehl <admin@gregorboehl.com>
2023-02-18 13:39:03 +0100

    minor fixes

--- econpizza/examples/__init__.py
+++ econpizza/examples/__init__.py
@@ -4,9 +4,9 @@ import os
 
 pth = os.path.dirname(__file__)
 
-nk = os.path.join(pth, "nk.yaml")
-bh = os.path.join(pth, "bh.yaml")
-dsge = os.path.join(pth, "dsge.yaml")
-hank = os.path.join(pth, "hank_with_comments.yaml")
-hank_labor = os.path.join(pth, "hank_labor.yaml")
-hank2 = os.path.join(pth, "hank2.yaml")
+nk = os.path.join(pth, "nk.yml")
+bh = os.path.join(pth, "bh.yml")
+dsge = os.path.join(pth, "dsge.yml")
+hank = os.path.join(pth, "hank_with_comments.yml")
+hank_labor = os.path.join(pth, "hank_labor.yml")
+hank2 = os.path.join(pth, "hank2.yml")

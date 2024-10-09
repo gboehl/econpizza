23115fcb4d56e57a7ee794b52e8c01f198b4d704
Gregor Boehl <admin@gregorboehl.com>
2024-10-09 15:14:15 +0200

    fix labor in EGM

--- econpizza/examples/hank_sam_functions.py
+++ econpizza/examples/hank_sam_functions.py
@@ -24,21 +24,23 @@ def egm_init(a_grid, skills_grid):
     return jnp.ones((skills_grid.shape[0], a_grid.shape[0]))*1e-2
 
 
-def egm_step(Wa_p, a_grid, skills_grid, w, n, T, xi, R, beta, sigma_c):
+def egm_step(Wa_p, a_grid, skills_grid, w, T, xi, R, beta, sigma_c):
     """A single backward step via EGM
     """
 
     # MUC as implied by next periods value function
     ux_nextgrid = beta * Wa_p
-    
+
     # benefits
     zeros_vector = jnp.zeros((1, len(skills_grid)-1))
     xi_reshaped = jnp.array([[xi]])
-    benefit_schedule_temp = jnp.concatenate([xi_reshaped, zeros_vector], axis=1)
+    benefit_schedule_temp = jnp.concatenate(
+        [xi_reshaped, zeros_vector], axis=1)
     benefit_schedule = benefit_schedule_temp[0]
 
     # labour income and consumption
-    labor_inc = skills_grid[:, None]*n*w + benefit_schedule[:, None]
+    # labor_inc = skills_grid[:, None]*n*w + benefit_schedule[:, None]
+    labor_inc = skills_grid[:, None]*w + benefit_schedule[:, None]
     c_nextgrid = ux_nextgrid**(-1/sigma_c)
 
     # get consumption in grid space
@@ -48,7 +50,7 @@ def egm_step(Wa_p, a_grid, skills_grid, w, n, T, xi, R, beta, sigma_c):
 
     # get todays distribution of assets
     a = rhs + labor_inc + T[:, None] - c
-    
+
     # fix consumption and labor for constrained households
     c = jnp.where(a < a_grid[0], labor_inc + rhs +
                   T[:, None] - a_grid[0], c)
@@ -107,11 +109,13 @@ def rouwenhorst_grid_from_stationary(sigma, stationary_distribution):
 def get_sam_grid_from_stationary(sigma, stationary_distribution):
     s = jnp.linspace(-1, 1, len(stationary_distribution)-1)
     s *= (sigma / jnp.sqrt(variance(s, stationary_distribution[1:])))
-    scaled_grid = jnp.exp(s) / jnp.sum(stationary_distribution[1:] * jnp.exp(s))
-    
-    augmented_grid = jnp.concatenate([jnp.array([0.0]), scaled_grid]) # unemployment state
-    return augmented_grid 
- 
+    scaled_grid = jnp.exp(
+        s) / jnp.sum(stationary_distribution[1:] * jnp.exp(s))
+
+    augmented_grid = jnp.concatenate(
+        [jnp.array([0.0]), scaled_grid])  # unemployment state
+    return augmented_grid
+
 
 def stationary_distribution(T):
     """Find invariant distribution of a Markov chain by unit eigenvector.
@@ -126,44 +130,45 @@ def stationary_distribution(T):
 
     return unit_ev.real / unit_ev.real.sum()
 
-    
+
 def sam_markov(rho, sigma, N, find_rate, separate_rate):
-    """Obtain transition matrix, stationary distribution and skill levels with 
+    """Obtain transition matrix, stationary distribution and skill levels with
     unemployment state (which corresponds to skill = 0).
     """
-    # Get transition matrix and its stationary distribution for the employed 
+    # Get transition matrix and its stationary distribution for the employed
     # agents switching skills
     _, pi, Pi = markov_rouwenhorst(rho, sigma, N)
-    
+
     # Get number of states of standard grid
     pi_len = len(pi)
-    
+
     # Create new transition matrix
     new_row_1 = jnp.array([1 - find_rate])
     new_row_2 = find_rate * pi
     new_row = jnp.concatenate((new_row_1, new_row_2), axis=0)
     new_row = new_row.reshape(1, len(new_row))
-    
+
     new_col = separate_rate * jnp.array((jnp.ones(pi_len)))
     new_col = new_col.reshape(len(new_col), 1)
-    scaled_Pi = (1 - separate_rate) * Pi  # Scale the original transition matrix
+    # Scale the original transition matrix
+    scaled_Pi = (1 - separate_rate) * Pi
     new_lower = jnp.concatenate((new_col, scaled_Pi), axis=1)
-    
+
     Pi_new = jnp.concatenate((new_row, new_lower), axis=0)
     row_sums = jnp.sum(Pi_new, axis=1, keepdims=True)
     sam_Pi = Pi_new / row_sums
-    
+
     # Get new stationary distribution
     sam_pi = stationary_distribution(jnp.array(sam_Pi.T))
-    
+
     # Get new grid of skill levels, incl. unemployment state
     sam_y = get_sam_grid_from_stationary(sigma, sam_pi)
-    
+
     return sam_y, sam_pi, pi, sam_Pi
 
 
-def get_ur(dist):
+def get_unemployment(dist):
     """Calculate unemployment rate, given the distribution of households over states.
     """
-    dist_over_skills = jnp.sum(dist, axis = 1)
+    dist_over_skills = jnp.sum(dist, axis=1)
     return dist_over_skills[0]

20078fb10e7d8694a8a6f03160afe979e0e46cd5
Gregor Boehl <admin@gregorboehl.com>
2025-03-30 17:13:38 +0200

    add hönk yaml

--- econpizza/examples/__init__.py
+++ econpizza/examples/__init__.py
@@ -9,8 +9,11 @@ bh = os.path.join(pth, "bh.yml")
 dsge = os.path.join(pth, "dsge.yml")
 tank = os.path.join(pth, "tank.yml")
 ghls = os.path.join(pth, "ghls.yml")
+
 hank = os.path.join(pth, "hank_with_comments.yml")
 hank_labor = os.path.join(pth, "hank_labor.yml")
+hank_banking = os.path.join(pth, "hank_banking.yml")
+hank_climate = os.path.join(pth, "hank_climate.yml")
+
 hank2 = os.path.join(pth, "hank2.yml")
 hank2_no_capital = os.path.join(pth, "hank2_no_capital.yml")
-hank_banking = os.path.join(pth, "hank_banking.yml")
